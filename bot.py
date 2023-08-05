a3adc4e926c2ee6677cbe42e57efab6b4c66271f
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-08-05 15:40:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
-import os
 import logging
-from config import Config
-from pyrogram import Client as Mxplayer
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+import os
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+
+
 
 if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
@@ -12,8 +20,6 @@ if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
     )
-    app = pyrogram.Client(
-        "Mxplayer",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
